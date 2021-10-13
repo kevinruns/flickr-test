4d5024a13cc5c7f39db1cfb71a2bf560b7c4874e
Kevin Lenehan <klenehan123@gmail.com>
2021-10-13 23:42:10 +0100

    basic working version

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+
+  require 'flickr'
+
+  def index
+    begin
+      flickr = Flickr.new
+      unless params[:user_id].blank?
+        @photos = flickr.photos.search(user_id: params[:user_id])
+      end
+    rescue StandardError => e
+      flash[:alert] = "#{e.class}: #{e.message}. Please try again..."
+      redirect_to root_path
+    end
+
+  end
+
+end
